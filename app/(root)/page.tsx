99865b28301a8867dd9bcad1f184a38ff22d1fa5
vinay_hv <vinayhv66@gmail.com>
2025-06-14 23:25:30 +0530

    user component form is used

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 import SearchForm from "../components/SearchForm";
-export default function Home(){
-  return(
-    <>
-    <section className="pink_container">
-    <h1 className="heading">Pitch your startup,<br />
-connect with entrepreneurs</h1>
-    <p className="subheading !max-w-3xl">
-submit your startup to get noticed in virtual competitions and events
-    </p>
-    <SearchForm/>
-    </section>
-    </>
-  )
+
+export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
+    const query = (await searchParams).query;
+
+    return (
+        <section className="pink_container">
+            <h1 className="heading">
+                Pitch Your Startup, <br />
+                Connect with Entrepreneurs
+            </h1>
+            <p className="sub-heading !max-w-3xl">
+                Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+            </p>
+            <SearchForm query={query}/>
+        </section>
+    );
 }
--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,20 +1,24 @@
-import Form from "next/form";
-const SearchForm = () => {
-    const query = 'test'
-    const reset = () => {
-        const form= document.querySelector(selectors:'.search-form') as HTMLFormElement;
-    
-    if(form) form.reset();
-}
-    return(
-        <Form action="/" scroll={false} className="search-form">
-            <input name="query" defaultValue={query} className="search-input"  placeholder="Search for a startup" />
-            <div className="flex gap-2"> 
-                {query && (
-                <button type="reset" onClick={reset}>Search</button>
+import { Search } from "lucide-react";
+import SearchFormReset from "./SearchFormReset";
+
+const SearchForm= ({query}:{query?:string}) => {
+
+    return (
+        <form action="/" className="search-form">
+            <input
+                name="query"
+                defaultValue={query}
+                className="search-input"
+                placeholder="Search for a startup"
+            />
+            <div className="flex gap-2">
+                {query && (<SearchFormReset/>)}
+                <button type="submit" className="search-btn text-white">
+                    <Search className="size-5"/>
+                    </button>
+            
             </div>
-        </Form>
-    )
-}
+        </form>
+    );
 }
 export default SearchForm;
