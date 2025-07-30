720b24bb0749e90828b5a2d140f949af130660b7
vinay_hv <vinayhv66@gmail.com>
2025-07-30 13:06:51 +0530

    errors resolved 2

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async () => {
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <Image src="/logo.png" alt="logo" width={144} height={30} />
+          <Image src="blob:https://www-towrco-in.filesusr.com/0bc31200-522d-443a-9557-a4e2504e3957" alt="logo" width={144} height={30} />
         </Link>
 
         <div className="flex items-center gap-5 text-black">
